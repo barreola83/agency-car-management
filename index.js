331720459ccedd1a241a971abc96f1d65ff89da9
barreola83 <barreola83@gmail.com>
2018-05-29 19:16:25 -0500

    Node deleted from project.

--- index.js
+++
@@ -1,7 +0,0 @@
-let http = require('http');
-
-http.createServer(function (req, res) {
-    let pathname = url.parse(req.url).pathname;
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end();
-}).listen(8080);
